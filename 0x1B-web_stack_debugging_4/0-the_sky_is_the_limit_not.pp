da0e8dab6630bad00602e390895a23fa4d577f6e
mohamed <mohamedkharashy27@yahoo.com>
2023-11-13 01:18:26 -0800

    1

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,12 @@
+# command to fix the problem of high amount or requests
+
+exec {'replace':
+  provider => shell,
+  command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
+  befoe    => Exec['restart'],
+}
+
+exec {'restart':
+  provider => shell,
+  command  => 'sudo service nginx restart',
+}
